d9c6aa87e04a5a00e1f8a44b7a28081a141dbeee
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-06-28 01:11:58 +0900

    Removed simple_calendar.

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,11 @@
 module ReservationsHelper
+  def times
+    times = []
+    (0..23).each do |hour|
+      [0, 30].each do |minute|
+        times << "#{format("%02d", hour)}:#{format("%02d", minute)}"
+      end
+    end
+    times
+  end
 end
