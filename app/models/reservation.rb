876dc87a207520df821cdb5bb21d973c21c07c10
Atsuya Uchida <atsuya_eagle@outlook.com>
2022-06-30 05:56:24 +0900

    Added validation which prevent end_at from being before start_at.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,9 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   attr_accessor :date, :start_time, :end_time
+  validate :start_at_should_be_before_end_at
+
+  def start_at_should_be_before_end_at
+    errors.add(:start_at, "can't be after end at") if start_at > end_at
+  end
 end
